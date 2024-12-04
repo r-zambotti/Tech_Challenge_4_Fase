d054b01731edfdcee7af4003a9f558f6d8e85313
Victor Novais <victor.ndo@outlook.com>
2024-12-03 23:40:28 -0300

    Conclusão

--- app/pages/conclusao.py
+++ app/pages/conclusao.py
@@ -11,29 +11,57 @@ with open('assets/css/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 with st.container():
-    st.header(":orange[Conclusão]")
+    st.header(":rainbow[Conclusão]")
+st.markdown('''
+            <p style="font-size: 16px"><br>        
+            Este projeto abordou a análise e previsão dos preços históricos do petróleo Brent, com o objetivo de fornecer insights relevantes para a 
+            tomada de decisão estratégica e desenvolver uma solução preditiva utilizando ferramentas modernas de Machine Learning<br>
+            </p>         
+''',unsafe_allow_html=True)
 
-    st.subheader(":gray[Análise dos Resultados e Eficácia do Modelo LSTM na Previsão do Preço do Petróleo]", divider="orange")
-    st.markdown(
-        """
-        Dos resultados obtidos, observamos um R² de 86%, o que indica que o modelo está acertando aproximadamente 86% das previsões realizadas. Esse valor é considerado alto, pois está próximo de 100%, sugerindo que o modelo possui uma capacidade significativa de explicar a variabilidade nos dados e de fazer previsões precisas do preço do petróleo. Além disso, temos valores de MSE, MAE e RMSE próximos de zero, o que é bom. Essas métricas indicam que as previsões do modelo têm uma dispersão muito baixa em relação aos valores reais, o que confirma a sua eficácia na previsão do preço do petróleo. Em suma, os resultados obtidos destacam a robustez e a precisão do modelo na tarefa de prever o preço do petróleo com base nos dados fornecidos.
-        """
-    )
+st.subheader(":orange[Pilares de Desenvolvimento]", divider="orange")
 
-    st.subheader(":gray[Impacto Prático]", divider="orange")
-    st.markdown(
-        """
-        Os resultados obtidos com o modelo LSTM têm um impacto significativo no contexto prático da tomada de decisões no mercado de petróleo. Uma previsão precisa do preço do petróleo é crucial para uma variedade de setores, incluindo finanças, energia e comércio internacional. Com base nos resultados deste estudo, os tomadores de decisão podem tomar medidas mais informadas para mitigar riscos, otimizar investimentos e planejar estratégias de negócios.
-        """
-    )
+st.subheader(":gray[Análise Exploratória de Dados]")
+st.markdown('''
+            <p style="font-size: 16px"><br>        
+            A análise histórica permitiu identificar fatores influenciadores dos preços, como crises econômicas globais, 
+            eventos geopolíticos, tensões entre paises e pandemia mundial.<br>
+            <br> Foram destacados momentos de impacto significativo, como a crise financeira de 2008 e os efeitos da COVID-19.</br>
+            </p>         
+''',unsafe_allow_html=True)
 
-    st.subheader(":gray[Aprimoramentos Futuros]", divider="orange")
-    st.markdown(
-        """
-        Embora os resultados até agora sejam promissores, há espaço para aprimoramentos adicionais no modelo LSTM. Algumas áreas de aprimoramento futuro podem incluir:\n
-        **1. Incorporação de Mais Dados:** O desempenho do modelo pode ser melhorado pela inclusão de uma gama mais ampla de dados, como dados macroeconômicos, indicadores geopolíticos e informações sobre a demanda global por petróleo.\n
-        **2. Otimização de Parâmetros:** A otimização dos hiperparâmetros do modelo LSTM pode ajudar a melhorar ainda mais sua precisão e generalização.\n
-        **3. Implementação de Técnicas de Pós-processamento:** Técnicas de pós-processamento, como ensemble learning ou técnicas de correção de viés, podem ser aplicadas para melhorar a robustez e a estabilidade das previsões do modelo.\n
-        Essas sugestões de aprimoramentos futuros podem ajudar a aprimorar ainda mais a capacidade do modelo LSTM de prever o preço do petróleo e ampliar seu impacto prático em cenários do mundo real.  
-        """
-    )
+st.subheader(":gray[Desenvolvimento do Modelo Preditivo]")
+st.markdown('''
+            <p style="font-size: 16px"><br>        
+            Um modelo de previsão de séries temporais foi implementado para estimar os preços diários do petróleo. 
+            As métricas de avaliação indicaram que o modelo é funcional e atende aos requisitos iniciais, apesar das limitações inerentes à volatilidade do mercado.</br>
+            <br>Métricas como MAE e MAPE demonstraram que o modelo é confiável dentro do escopo proposto.</br>
+            </p>         
+''',unsafe_allow_html=True)
+
+st.subheader(":gray[Dashboard Interativo]")
+st.markdown('''
+            <p style="font-size: 16px"><br>        
+            Um dashboard foi desenvolvido para apresentar insights de maneira clara e interativa, 
+            conectando os resultados da análise com o storytelling necessário para apoiar decisões estratégicas.</br>
+            </p>         
+''',unsafe_allow_html=True)
+
+st.subheader(":gray[Plano de Deploy]")
+st.markdown('''
+            <p style="font-size: 16px"><br>        
+            Foi criado um protótipo funcional utilizando o Streamlit, 
+            permitindo que as previsões sejam acessadas em um ambiente simples e amigável, pronto para ser utilizado em produção com aprimoramentos futuros.</br>
+            </p>         
+''',unsafe_allow_html=True)
+
+st.subheader(":orange[Resultados e Contribuições]", divider="orange")
+st.markdown('''
+            <p style="font-size: 16px"><br>        
+            O projeto atendeu aos objetivos iniciais, oferecendo:</br>
+            <br>
+            - Insights estratégicos baseados nos dados históricos de preços do petróleo.<br>
+            - Ferramentas preditivas que permitem estimar valores futuros e ajudam no planejamento estratégico.<br>
+            - Uma solução interativa que une storytelling e visualização de dados para facilitar a tomada de decisão.<br>
+            </p>         
+''',unsafe_allow_html=True)
